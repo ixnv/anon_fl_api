b108160c964393cdb22b6512696cd5279afc5eed
Ramil <autechregescom@gmail.com>
2017-02-27 13:27:30 +0300

    *: notify api - communicate with JWT

--- anon_fl/notify_api.py
+++ anon_fl/notify_api.py
@@ -1,3 +1,4 @@
+import jwt
 from django.conf import settings
 import requests
 
@@ -9,16 +10,22 @@ ORDER_APPLICATION_APPROVED = 'ORDER_APPLICATION_APPROVED'
 ORDER_APPLICATION_DECLINED = 'ORDER_APPLICATION_DECLINED'
 
 
+def generate_jwt():
+    return jwt.encode({}, settings.JWT_NOTIFY_SECRET)
+
+
 def notify(user_ids, entity_id, key, data):
     return requests.post(BASE_URL + '/notify', json={
         'user_ids': user_ids,
         'entity_id': entity_id,
         'key': key,
-        'data': data
+        'data': data,
+        'token': generate_jwt()
     })
 
 
 def read_notifications(user_id):
     return requests.post(BASE_URL + '/notifications/read', json={
-        'user_id': user_id
+        'user_id': user_id,
+        'token': generate_jwt()
     })
